ee95689fc5af66956b66560067db50d8ac9a66a7
Imanuel <imanuel.putra@ti.ukdw.ac.id>
2024-11-30 15:09:10 +0700

    update welcome

--- app/src/main/java/com/capstone/sweettrack/view/ui/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/capstone/sweettrack/view/ui/welcome/WelcomeFragment.kt
@@ -1,15 +1,26 @@
 package com.capstone.sweettrack.view.ui.welcome
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Build
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.coding.sweettrack.R
+import com.coding.sweettrack.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
 
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
 
     private val viewModel: WelcomeViewModel by viewModels()
 
@@ -17,12 +28,87 @@ class WelcomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupView()
+        playAnimation()
+        setupAction()
+        observeViewModel()
     }
 
+    private fun setupView() {
+        val window = requireActivity().window
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        (requireActivity() as? AppCompatActivity)?.supportActionBar?.hide()
+    }
+
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.RESTART
+        }.start()
+
+        val login = ObjectAnimator.ofFloat(binding.loginButton, View.ALPHA, 1f).setDuration(100)
+        val signup = ObjectAnimator.ofFloat(binding.signupButton, View.ALPHA, 1f).setDuration(100)
+        val title = ObjectAnimator.ofFloat(binding.titleTextView, View.ALPHA, 1f).setDuration(100)
+
+        val together = AnimatorSet().apply {
+            playTogether(login, signup)
+        }
+
+        AnimatorSet().apply {
+            playSequentially(title, together)
+            start()
+        }
+    }
+
+    private fun setupAction() {
+        binding.loginButton.setOnClickListener {
+            viewModel.onLoginClicked()
+        }
+
+        binding.signupButton.setOnClickListener {
+            viewModel.onSignupClicked()
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.navigateTo.observe(viewLifecycleOwner) { navigateTo ->
+            val navController = findNavController()
+            when (navigateTo) {
+                WelcomeViewModel.NavigateTo.LOGIN -> {
+                    navController.navigate(R.id.action_welcomeFragment_to_loginFragment)
+                    viewModel.navigationDone()
+                }
+
+                WelcomeViewModel.NavigateTo.SIGNUP -> {
+                    navController.navigate(R.id.action_loginFragment_to_fragmentSignUp)
+                    viewModel.navigationDone()
+                }
+
+                null -> {
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
