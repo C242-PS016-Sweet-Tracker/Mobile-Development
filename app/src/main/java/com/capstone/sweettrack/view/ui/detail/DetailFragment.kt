b683cdf4dc86b3ffeaef5b8ec1c9e18c982b45c5
Imanuel <imanuel.putra@ti.ukdw.ac.id>
2024-12-02 14:43:28 +0700

    update

--- app/src/main/java/com/capstone/sweettrack/view/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/capstone/sweettrack/view/ui/detail/DetailFragment.kt
@@ -1,15 +1,20 @@
 package com.capstone.sweettrack.view.ui.detail
 
 import DetailViewModel
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.coding.sweettrack.R
 import com.coding.sweettrack.databinding.FragmentDetailBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class DetailFragment : Fragment() {
@@ -31,6 +36,8 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupView()
+
         // Observe status favorite untuk memperbarui ikon
         viewModel.isFavorite.observe(viewLifecycleOwner, Observer { isFavorite ->
             if (isFavorite) {
@@ -45,6 +52,37 @@ class DetailFragment : Fragment() {
             viewModel.toggleFavorite()
         }
 
+    }
+
+    private fun setupView() {
+        val window = requireActivity().window
+        val decorView = window.decorView
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+            window.insetsController?.systemBarsBehavior =
+                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        } else {
+            @Suppress("DEPRECATION")
+            decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
+        }
+        (requireActivity() as? AppCompatActivity)?.supportActionBar?.show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView?.visibility = View.GONE
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView?.visibility = View.VISIBLE
     }
 
     override fun onDestroyView() {
