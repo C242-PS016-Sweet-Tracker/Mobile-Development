b668225e72bbf00509519d8c2696e9831b4e426a
ameliaetsa <maria.ni@ti.ukdw.ac.id>
2024-12-09 06:49:17 +0700

    rekomendasi
    mayaan

--- app/src/main/java/com/capstone/sweettrack/data/remote/api/ApiConfig.kt
+++ app/src/main/java/com/capstone/sweettrack/data/remote/api/ApiConfig.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
     companion object {
 //        private const val BASE_URL = BuildConfig.BASE_URL // Ganti dengan pake URL API
-//        private const val BASE_URL = "http://192.168.249.243:8080/" // endpoint sementara isp public
+//        private const val BASE_URL = "http://192.168.0.46:8080/" // endpoint sementara isp public
 
         fun getApiService(): ApiService {
             val loggingInterceptor =
